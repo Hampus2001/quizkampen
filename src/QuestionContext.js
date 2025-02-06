93790e707d42528cc86be8d1f2f64747e68049bb
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-02-06 15:21:19 +0100

    updated login

--- src/QuestionContext.js
+++ src/QuestionContext.js
@@ -5,14 +5,10 @@ export const HandleQuestionContext = createContext([]);
 export default function QuestionContext({ children }) {
   const [question, setQuestion] = useState([
     {
-      question: "Hur många bor i Sverige?",
-      alternatives: [
-        "8 miljoner",
-        "9,5 miljoner",
-        "10,5 miljoner",
-        "12 miljoner",
-      ],
-      answer: "10,5 miljoner",
+      question:
+        "Which of the following is the largest planet in our solar system?",
+      alternatives: ["Earth", "Mars", "Jupiter", "Saturn"],
+      answer: "Jupiter",
     },
     {
       question: "Hur många bor i Sverige?",
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,8 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { HandleQuestionContext } from "@/QuestionContext";
 
 export default function Admin() {
   const { question, setQuestion } = useContext(HandleQuestionContext);
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUsername] = useState("Meta");
+  const [password, setPassword] = useState("Quizkampen");
+  const [inputUsername, setInputUsername] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+
+  function handleLogin() {
+    if (userName === inputUsername && password === inputPassword) {
+      setLoggedIn(true);
+    } else {
+      alert("Invalid login!");
+    }
+  }
 
   const displayQuestions = [];
 
@@ -10,13 +24,13 @@ export default function Admin() {
     displayQuestions.push(
       <div
         key={i}
-        className="flex flex-col items-center w-2/5 gap-3 my-10 bg-white text-black rounded-xl"
+        className="flex flex-col items-center w-2/4 gap-3 my-10 bg-white text-black rounded-xl"
       >
         <h2 className="w-full p-10 text-5xl text-center font-bold bg-secondary rounded-t-xl">
           Question {i + 1}
         </h2>
         <input
-          className="bg-white text-5xl px-5 w-full text-center"
+          className="bg-white text-5xl px-5 w-full text-center outline-none"
           value={question[i].question}
           onChange={(e) => {
             const newQuestions = [...question];
@@ -32,7 +46,7 @@ export default function Admin() {
                 Alternative {index + 1} :{" "}
               </p>
               <input
-                className="bg-white w-full p-2 rounded-lg"
+                className="bg-white w-full p-2 rounded-lg  outline-none"
                 key={index}
                 value={alternative}
                 onChange={(e) => {
@@ -47,7 +61,7 @@ export default function Admin() {
         <div className="flex w-full items-center justify-center gap-5 p-5 text-3xl bg-secondary rounded-b-lg">
           <p>Answer : </p>
           <input
-            className="bg-white p-2 rounded-lg"
+            className="bg-white p-2 rounded-lg  outline-none"
             value={question[i].answer}
             onChange={(e) => {
               const newQuestions = [...question];
@@ -62,8 +76,39 @@ export default function Admin() {
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-7xl font-bold p-10">CONTROLL CENTER</h1>
-      {displayQuestions}
+      {loggedIn ? (
+        <div className="flex flex-col items-center">
+          <h1 className="text-7xl font-bold p-10">CONTROLL CENTER</h1>
+          {displayQuestions}
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center mt-60">
+          <div className="p-10 bg-primary rounded-xl">
+            <div className="flex flex-col">
+              <input
+                className="p-5 bg-white text-black text-2xl rounded-xl outline-none"
+                type="text"
+                placeholder="Username:"
+                onChange={(e) => setInputUsername(e.target.value)}
+              />
+              <br />
+              <input
+                className="p-5 bg-white text-black text-2xl rounded-xl outline-none"
+                type="password"
+                placeholder="Password:"
+                onChange={(e) => setInputPassword(e.target.value)}
+              />
+              <br />
+              <button
+                className="bg-base-300 p-5 text-2xl rounded-xl"
+                onClick={() => handleLogin()}
+              >
+                LOG IN
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
