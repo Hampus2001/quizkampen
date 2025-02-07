87c805110c3c328a648520c03a237f648ea2c86e
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-02-07 14:32:07 +0100

    Fixed error with admin Page

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -183,6 +183,7 @@ export default function Admin() {
                 onClick={() => {
                   setUsername(updatedUsername);
                   setPassword(updatedPassword);
+                  setShowModal(false);
                 }}
               >
                 UPDATE LOGIN
